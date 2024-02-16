b567d90e7d48ab828aebb92feafaee3ec62efc3e
omsalunke0852 <103877025+om0852@users.noreply.github.com>
2024-02-16 06:42:25 +0530

    link icon added

--- src/app/component/Project.js
+++ src/app/component/Project.js
@@ -4,6 +4,7 @@ import "../css/about.css";
 import zomato from "../img/zomato.png"
 import projectmanagement from "../img/projectManagement.png";
 import bluechipart from "../img/bluechipart.png"
+import linkIcon from "../img/icons8-link.gif"
 export default function Project() {
     return (
         <div className="about-section">
@@ -14,7 +15,7 @@ export default function Project() {
                     <div class="layer">
                         <h3>Zomato Clone</h3>
                         <p>This is just static Zomato Clone develop using html,css,js</p>
-                        <a href="https://github.com/om0852/Zomato-clone"> <i class="fa-solid fa-link"></i></a>
+                        <a href="https://github.com/om0852/Zomato-clone"> <img src={linkIcon.src} alt="link" /></a>
                     </div>
                 </div>
                 <div class="work">
@@ -22,7 +23,7 @@ export default function Project() {
                     <div class="layer">
                         <h3>Bluechipart Website</h3>
                         <p>This website allow user to buy a portfolio that consist stock,cypto and painting assest Features:-Instant buy and sell,you can buy any amount you want,top portfolios.this website is develop using,Next.js,Express.js,Web3,Solodity,Mongodb</p>
-                        <a href="https://github.com/om0852/Assest-Pooling-and-Fractional-Ownership"> <i class="fa-solid fa-link"></i></a>
+                        <a href="https://github.com/om0852/Assest-Pooling-and-Fractional-Ownership"> <a href="https://github.com/om0852/Zomato-clone"> <img src={linkIcon.src} alt="link" /></a></a>
                     </div>
                 </div>
 
@@ -31,7 +32,7 @@ export default function Project() {
                     <div class="layer">
                         <h3>Project Management website</h3>
                         <p>This webiste allow you to manage project in an company feature it includes are milestone updates,project tracking,access level.for development html,css,js,php,mysql</p>
-                        <a href="https://github.com/om0852/ProjectManagmentDsc"> <i class="fa-solid fa-link"></i></a>
+                        <a href="https://github.com/om0852/ProjectManagmentDsc"> <a href="https://github.com/om0852/Zomato-clone"> <img src={linkIcon.src} alt="link" /></a></a>
                     </div>
                 </div>
             </div>
